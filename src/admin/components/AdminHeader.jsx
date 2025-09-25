9edf08090417692f266162ada3ea817d1f343e45
Minu-Rose-Vazhappilly <minurosevazhappilly29@gmail.com>
2025-09-25 21:41:55 +0530

    #feat:Added Design of AdminHeade Component,AdminSideBar Component,SettingAdmin Page

--- src/admin/components/AdminHeader.jsx
+++ src/admin/components/AdminHeader.jsx
@@ -1,8 +1,26 @@
+import { faPowerOff } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+
 
 function AdminHeader() {
   return (
-    <div>AdminHeader</div>
+    <>
+      <div className='flex justify-between p-3'>
+        <div className='flex justify-center items-center '>
+          <img width={"50px"} height={"50px"} src="./logo.png" alt="" />
+          <h1 className='ms-2'>BOOKSTORE</h1>
+        </div>
+        <button className='flex items-center justify-center border border-black px-1 py-2 rounded'>
+          
+               <FontAwesomeIcon icon={faPowerOff} /> LOGOUT 
+          
+        </button>
+      </div>
+      <div className='bg-black flex md:items-center justify-center'>
+            <p className='text-white'>Welcome,  Admin!    You're all set to manage and monitor the system. Let’s get to work!</p>
+      </div>
+    </>
   )
 }
 
--- src/admin/components/AdminSideBar.jsx
+++ src/admin/components/AdminSideBar.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { faUser } from '@fortawesome/free-regular-svg-icons'
+import { faBook, faScrewdriverWrench, faBagShopping, faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useState } from 'react'
+
+
 
 function AdminSideBar() {
+  const [listStatus,setListstatus] = useState(false)
   return (
-    <div>AdminSideBar</div>
+    <div className='bg-sky-100 flex justify-center items-center flex-col px-23 py-30 '>
+     <div className='bg-gray-400 rounded-full text-white flex justify-center items-center text-3xl' style={{width:"70px",height:"70px"}}>
+      <FontAwesomeIcon icon={faUser} />
+     </div>
+     <p className='mt-2 text-xl'>Username</p>
+     <button onClick={()=>setListstatus(!listStatus)} className='text-2xl md:hidden '><FontAwesomeIcon icon={faBars} style={{color:"blue"}}/></button>
+     <div className={listStatus?'block mt-2':'md:block hidden mt-2'}>
+       <ul className=''  style={{ listStyleType: "circle" }}>
+          <li><FontAwesomeIcon icon={faBook} />Home</li>
+          <li><FontAwesomeIcon icon={faBook} />AllBooks</li>
+          <li><FontAwesomeIcon icon={faBagShopping} />Careers</li>
+          <li><FontAwesomeIcon icon={faScrewdriverWrench} />Settings</li>
+        </ul>
+     </div>
+    </div>
   )
 }
 
