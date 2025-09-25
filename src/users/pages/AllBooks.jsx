4ad7253e83e10fdfe12993eaabfbef7d7f0fd931
Minu-Rose-Vazhappilly <minurosevazhappilly29@gmail.com>
2025-09-25 11:03:54 +0530

    #feat:Added Design of View Book and Updated Design of AllBooks

--- src/users/pages/AllBooks.jsx
+++ src/users/pages/AllBooks.jsx
@@ -1,58 +1,104 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../components/Header'
 import Footer from '../../components/Footer'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function AllBooks() {
+  const [listStatus,setListstatus] = useState(false)
   return (
     <div>
       <Header/>
-     <>
-     <div className="flex justify-center items-center flex-col my-5">
+     
+     <div className="flex justify-center md:items-center flex-col my-5 p-3">
           <h1 className='text-3xl'>Collections</h1>
           <div className="flex">
             <input type="text" className="p-2 text-black rounded border border-gray-200 w-100 placeholder-gray-600" placeholder='Search by Title' />
             <button className='bg-blue-500 w-20'>Search</button>
           </div>
      </div>
-     <div className="grid grid-cols-3 p-5 md:px-40">
+     <div className="md:grid grid-cols-4 p-5 md:px-20 mb-10">
+
       <div className="col-span-1">
-        <h1 className='text-xl font-semibold'>Filter</h1>
-        <div>
-          <input type="radio" id='Literature' name='filter'/>
-          <label className='ms-3' htmlFor="Literature">Literature Fiction</label>
+       <div className='flex justify-between '>  
+          <h1 className='text-xl font-semibold'>Filter</h1>
+          <button onClick={()=>setListstatus(!listStatus)} className='text-2xl md:hidden'><FontAwesomeIcon icon={faBars} /></button>
+       </div>
+        <div className={listStatus?'block':'md:block hidden'}>
+          <div className='mt-3'>
+            <input type="radio" id='Literature' name='filter'/>
+            <label className='ms-3' htmlFor="Literature">Literature Fiction</label>
+          </div>
+          <div>
+            <input type="radio" id='philosphy' name='filter'/>
+            <label className='ms-3' htmlFor="Literature">Philosophy</label>
+          </div>
+          <div>
+            <input type="radio" id='romanace' name='filter'/>
+            <label className='ms-3' htmlFor="Literature">Romance</label>
+          </div>
+          <div>
+            <input type="radio" id='mystery' name='filter'/>
+            <label className='ms-3' htmlFor="Literature">Mystery/Thriller</label>
+          </div>
+          <div>
+            <input type="radio" id='horror' name='filter'/>
+            <label className='ms-3' htmlFor="Literature">Horror</label>
+          </div>
+          <div>
+            <input type="radio" id='autobio' name='filter'/>
+            <label className='ms-3' htmlFor="Literature">Auto/Biography</label>
+          </div>
+          <div>
+            <input type="radio" id='self-help' name='filter'/>
+            <label className='ms-3' htmlFor="Literature">Self-Help</label>
+          </div>
+          <div>
+            <input type="radio" id='Politics' name='filter'/>
+            <label className='ms-3' htmlFor="Literature">Politics</label>
+          </div>
+          
         </div>
-        <div>
-          <input type="radio" id='Literature' name='filter'/>
-          <label className='ms-3' htmlFor="Literature">Philosophy</label>
+      </div>
+      <div className="col-span-3">
+        <div className="md:grid grid-cols-4">
+          <div className="shadow rounded p-3 mx-4">
+            <img  width={"100%"} height={"300px"} src="https://www.creativindiecovers.com/wp-content/uploads/2012/02/9780718155209.jpg" alt="" />
+            <div className='flex flex-col justify-center items-center'>
+            <h1 className='text-center '>Author</h1>
+            <h1 className='text-2xl'>Title</h1>
+            <button className='bg-blue-500 rounded px-4 py-1'>Buy</button>
+            </div>
+          </div>
+          <div className="shadow rounded p-3 mx-4">
+            <img  width={"100%"} height={"300px"} src="https://www.creativindiecovers.com/wp-content/uploads/2012/02/9780718155209.jpg" alt="" />
+            <div className='flex flex-col justify-center items-center'>
+            <h1 className='text-center '>Author</h1>
+            <h1 className='text-2xl'>Title</h1>
+            <button className='bg-blue-500 rounded px-4 py-1'>Buy</button>
+            </div>
+          </div>
+          <div className="shadow rounded p-3 mx-4">
+            <img  width={"100%"} height={"300px"} src="https://www.creativindiecovers.com/wp-content/uploads/2012/02/9780718155209.jpg" alt="" />
+            <div className='flex flex-col justify-center items-center'>
+            <h1 className='text-center '>Author</h1>
+            <h1 className='text-2xl'>Title</h1>
+            <button className='bg-blue-500 rounded px-4 py-1'>Buy</button>
+            </div>
+          </div>
+          <div className="shadow rounded p-3 mx-4">
+            <img  width={"100%"} height={"300px"} src="https://www.creativindiecovers.com/wp-content/uploads/2012/02/9780718155209.jpg" alt="" />
+            <div className='flex flex-col justify-center items-center'>
+            <h1 className='text-center '>Author</h1>
+            <h1 className='text-2xl'>Title</h1>
+            <button className='bg-blue-500 rounded px-4 py-1'>Buy</button>
+            </div>
+          </div>
         </div>
-        <div>
-          <input type="radio" id='Literature' name='filter'/>
-          <label className='ms-3' htmlFor="Literature">Romance</label>
-        </div>
-        <div>
-          <input type="radio" id='Literature' name='filter'/>
-          <label className='ms-3' htmlFor="Literature">Mystery/Thriller</label>
-        </div>
-        <div>
-          <input type="radio" id='Literature' name='filter'/>
-          <label className='ms-3' htmlFor="Literature">Horror</label>
-        </div>
-        <div>
-          <input type="radio" id='Literature' name='filter'/>
-          <label className='ms-3' htmlFor="Literature">Auto/Biography</label>
-        </div>
-        <div>
-          <input type="radio" id='Literature' name='filter'/>
-          <label className='ms-3' htmlFor="Literature">Self-Help</label>
-        </div>
-        <div>
-          <input type="radio" id='Literature' name='filter'/>
-          <label className='ms-3' htmlFor="Literature">Politics</label>
-        </div>
-        
       </div>
      </div>
-     </>
+     
+     
       <Footer/>
       </div>
   )
--- src/users/pages/ViewBook.jsx
+++ src/users/pages/ViewBook.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import Header from '../components/Header'
+import Footer from '../../components/Footer'
+import { faEye } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBackward } from '@fortawesome/free-solid-svg-icons'
 
 function ViewBook() {
   return (
-    <div>ViewBook</div>
+    <div>
+      <Header/>
+      <div className='md:m-10 m-5'>ViewBook
+        <div className="border p-5 shadow border-gray-200">
+          <div className='md:grid grid-cols-4 gap-x-10'>
+            <div className="col-span-1">
+              <img className='w-full' src="https://www.creativindiecovers.com/wp-content/uploads/2012/02/9780718155209.jpg" alt="book" />
+            </div>
+            <div className='col-span-3'>
+                <div className='flex justify-between'>
+                  <h1 className="text-xl font-bold r">Title</h1>
+                  <button className='text-gray-400'><FontAwesomeIcon icon={faEye}/></button>
+                </div>
+                <p className='my-3 text-blue-700'>Author</p>
+                <div className='md:grid grid-cols-3 gap-5 my-10'>
+                  <p className='font-bold'>Publisher:demo</p>
+                  <p className='font-bold'>Language:demo</p>
+                  <p className='font-bold'>No.og Pages</p>
+                  <p className='font-bold'>Seller Mail:demo</p>
+                  <p className='font-bold'>Real Price:demo</p>
+                  <p className='font-bold'>ISBN:demo</p>
+                </div>
+                <div className="md:my-10 my-4">
+                  <p className='font-bold text-lg'>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil, atque sapiente! Harum, eos maxime tenetur officia dignissimos inventore nobis unde nam asperiores delectus ad quibusdam rem quam molestias! Cupiditate, praesentium.
+                  </p>
+                </div>
+                <div className='flex justify-end'>
+                      <button className='bg-blue-900 text-white p-2 rounded'><FontAwesomeIcon icon={faBackward} />Back</button>
+                      <button className='bg-green-900 text-white ms-5 p-2 rounded'>Buy $123</button>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer/>
+    </div>
   )
 }
 
